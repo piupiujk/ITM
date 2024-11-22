0d7037ef8db2fdc153948393c42bdeec3e227074
piupiujk <docs4p@yandex.ru>
2024-11-22 11:26:26 +0500

    Сделал третью часть

---
+++ data_structures/third_part.py
@@ -0,0 +1,59 @@
+# 3 ЧАСТЬ
+# 1 задача
+a = 3
+plus = a > 0
+print('Число положительное', plus)
+
+# 2 задача
+a = 7
+odd = a % 2 != 0
+print('Число нечетное:', odd)
+
+# 3 задача
+a = 4
+even = a % 2 == 0
+print('Число четное:', even)
+
+# 4 задача
+a = 5
+b = 3
+expression = a > 2 and b <= 3
+print('Выражение истинно', expression)
+
+# 5 задача
+a = 7
+b = 6
+expression = a >= 2 and b <= -2
+print('Выражение истинно', expression)
+
+# 6 задача
+a = 2
+b = 7
+c = 10
+expression = a < b < c
+print('Выражение истинно', expression)
+
+# 7 задача
+a = 7
+b = 4
+c = 9
+expression = a < b < c
+print('Выражение истинно', expression)
+
+# 8 задача
+a = 13
+b = 7
+odd = a % 2 != 0 and b % 2 != 0
+print('Каждое число нечетное', odd)
+
+# 9 задача
+a = 3
+b = 16
+odd = a % 2 != 0 or b % 2 != 0
+print('Хотя бы одно число нечетное', odd)
+
+# 10 задача
+a = 7
+b = 12
+odd = a % 2 != 0 and b % 2 == 0 or a % 2 == 0 and b % 2 != 0
+print('Ровно одно число нечетное', odd)
